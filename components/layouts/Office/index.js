54051e8e9df981cf0658804e81ee4a28754e68fc
Joseph Okafor <joptimalresource@live.com>
2022-09-18 08:36:31 +0100

    added some sections for individual positions

---
+++ components/layouts/Office/index.js
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./Office.module.css";
+// import { offices } from "data/offices";
+
+const Office = ({
+  offices = [],
+  persons = [],
+  personsTitle,
+  addButtonTitle = "Add region",
+  toggleAddModal = () => {},
+}) => {
+  return (
+    <div className={styles.container}>
+      {offices.length > 0 && (
+        <>
+          <div className={styles.topSection}>
+            <div className="office_button">Offices</div>
+          </div>
+          <section className={styles.middleSection}>
+            {offices.map((office, index) => {
+              {
+                return (
+                  <div className={styles.card} key={index}>
+                    <div className={styles.cardImage}>
+                      <Image
+                        src="/assets/images/rccglogo.png"
+                        width={150}
+                        height={150}
+                        layout="intrinsic"
+                        alt="card"
+                      />
+                    </div>
+                    <div className={styles.cardInfo}>{office?.title}</div>
+                  </div>
+                );
+              }
+            })}
+          </section>
+        </>
+      )}
+
+      {/* {persons.length > 0 && ( */}
+      <>
+        <div className={styles.ministersSection}>
+          <div className={styles.ministerTitle}>Provincial Leaders</div>
+          <div className={styles.addNewButton}>Add New Province</div>
+        </div>
+        <div className={styles.ministersContent}>
+          <div className={styles.minister}>
+            <img src="/assets/images/1.jpg" width="200px" />
+          </div>
+        </div>
+
+        {/* <section className={styles.middleSection}>
+            {persons.map((person, index) => {
+              {
+                return (
+                  person?.parentId === 1 && (
+                    <div className={styles.card} key={index}>
+                      <div className={styles.cardImage}>
+                        <Image
+                          src="/assets/images/rccglogo.png"
+                          width={150}
+                          height={150}
+                          layout="intrinsic"
+                          alt="card"
+                        />
+                      </div>
+                      <div className={styles.cardInfo}>{person?.name}</div>
+                    </div>
+                  )
+                );
+              }
+            })}
+          </section> */}
+      </>
+      {/* )} */}
+    </div>
+  );
+};
+
+export default Office;
--- components/pages/dashboard/office-of-go/index.js
+++ components/pages/dashboard/office-of-go/index.js
@@ -2,35 +2,42 @@ import Image from "next/image";
 import React from "react";
 import styles from "./OfficeOfGO.module.css";
 import { offices } from "data/offices";
+import Office from "components/layouts/Office";
 function OfficeOfGO({}) {
+  const info = offices.filter((data, index) => {
+    return data.parentId === 1;
+  });
   return (
-    <div className={styles.container}>
-      <div className={styles.topSection}>
-        <div className="office_button">Directories</div>
-      </div>
-      <section className={styles.middleSection}>
-        {offices.map((office, index) => {
-          {
-            return (
-              office?.parentId === 1 && (
-                <div className={styles.card} key={index}>
-                  <div className={styles.cardImage}>
-                    <Image
-                      src="/assets/images/rccglogo.png"
-                      width={150}
-                      height={150}
-                      layout="intrinsic"
-                      alt="card"
-                    />
-                  </div>
-                  <div className={styles.cardInfo}>{office?.title}</div>
-                </div>
-              )
-            );
-          }
-        })}
-      </section>
-    </div>
+    // <div className={styles.container}>
+    //   <div className={styles.topSection}>
+    //     <div className="office_button">Offices</div>
+    //   </div>
+    //   <section className={styles.middleSection}>
+    //     {offices.map((office, index) => {
+    //       {
+    //         return (
+    //           office?.parentId === 1 && (
+    //             <div className={styles.card} key={index}>
+    //               <div className={styles.cardImage}>
+    //                 <Image
+    //                   src="/assets/images/rccglogo.png"
+    //                   width={150}
+    //                   height={150}
+    //                   layout="intrinsic"
+    //                   alt="card"
+    //                 />
+    //               </div>
+    //               <div className={styles.cardInfo}>{office?.title}</div>
+    //             </div>
+    //           )
+    //         );
+    //       }
+    //     })}
+    //   </section>
+    // </div>
+    <>
+      <Office offices={info} />
+    </>
   );
 }
 
