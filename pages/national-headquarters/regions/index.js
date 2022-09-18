e45c1b9496f9de7bceec666f0e9c839b48bf6ba6
Joseph Okafor <joptimalresource@live.com>
2022-09-18 08:54:17 +0100

    added some sections for individual positions.

---
+++ pages/national-headquarters/regions/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { Fragment } from "react";
+import OfficeOfGO from "components/pages/dashboard/office-of-go";
+
+export default function Regions() {
+  return (
+    <Fragment>
+      <Head>
+        <title>RCCG Office Info</title>
+        <meta
+          name="description"
+          content="A portal for checking the RCCG hierarchy and personnels, vision and mission statement"
+        />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+      <OfficeOfGO />
+    </Fragment>
+  );
+}
